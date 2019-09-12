015f99f5fa53b065c8948e4ed28af92cb807b5ff
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-09-12 10:32:15 +0100

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./images/starWarsLogo.png";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Explore The Universe</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
